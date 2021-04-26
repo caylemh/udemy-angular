ed0fc8744c16f1003cc3465277f1ceef104c53fd
caylemh <caylemh@gmail.com>
2021-04-26 13:50:33 +0200

    Using 'ngStyle' Directive

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -11,9 +11,15 @@ export class ServerComponent implements OnInit {
 
   getServerStatus = () => this.serverStatus;
 
-  constructor() { }
+  constructor() { 
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
 
   ngOnInit(): void {
+  }
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
   }
 
 }
