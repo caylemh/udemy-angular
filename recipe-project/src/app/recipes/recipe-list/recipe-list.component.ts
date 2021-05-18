85cfc28d8ae12b7fe65eb6fb0928e786d26fcb1e
caylemh <caylemh@gmail.com>
2021-05-18 14:07:07 +0200

    - Creating the rest of the Components

--- recipe-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] =[
+    new Recipe('Lasagne', 
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit.', 
+    'https://photos.bigoven.com/recipe/hero/beef-lasagne-7b988b.jpg?h=500&w=500'),
+    new Recipe('Beef Strogonov', 
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit.', 
+    'https://photos.bigoven.com/recipe/hero/beef-stroganoff-69.jpg?h=500&w=500'),
+    new Recipe('Leek & Potatoe Soup', 
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit.', 
+    'https://photos.bigoven.com/recipe/hero/leek-and-potato-soup-f0aa16.jpg?h=500&w=500')
+  ];
 
   constructor() { }
 
--- recipe-project/src/app/shopping-list/shopping-list.component.ts
+++ recipe-project/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient("Stewing Beef", 8),
+    new Ingredient("Lasagne Pasta Sheets", 10),
+    new Ingredient("Potatoes", 6)
+  ];
 
   constructor() { }
 
